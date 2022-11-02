7aa74d912e54c33b22ec0c552213d849374e4b3c
3419 <3419@example.com>
2022-11-02 11:07:01 +0900

    routeみんなと一緒

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,9 @@
 Rails.application.routes.draw do
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-  resources 'users'
-
-  namespace :admin do
     resources :levels
     resources :records
     resources :users
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tasks do
-    collection do
-      get 'sort_deadline'
-      get 'narrow_down_status'
-    end
-  end
-  root 'tasks#index'
+
   root 'users#index'
   get 'sessions/new'
   get    '/login',  to: 'sessions#new'
