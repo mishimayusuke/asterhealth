37d5283f2d4fd20a4521f4912902b61212879961
3419 <3419@example.com>
2023-01-27 10:25:21 +0900

    seed追加しました

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Record.create(user_id: 1 ,step: 2000 ,recorded: "2023/01/14" )
-# Record.create(user_id: 1 ,step: 2000 ,recorded: "2023/01/15" )
-# Record.create(user_id: 1 ,step: 3000 ,recorded: "2023/01/16" )
-# Record.create(user_id: 1 ,step: 4000 ,recorded: "2023/01/17" )
-# Record.create(user_id: 1 ,step: 5000 ,recorded: "2023/01/18" )
-Record.create(user_id: 1 ,step: 4000 ,recorded: "2023/01/19" )
-Record.create(user_id: 1 ,step: 5000 ,recorded: "2023/01/20" )
+# User.create(mail:"student@gmail.com" ,password:"student" ,authority:0 ,nickname:"出商生徒" ,level_id:"1" ,level_stop:"あり")
+# User.create(mail:"teacher@gmail.com" ,password:"teacher" ,authority:1 ,nickname:"出商教師" ,level_id:"1" ,level_stop:"あり")
+
+# Record.create(user_id: 1 ,step: 2000 ,recorded: "2023/01/01" )
+# Record.create(user_id: 1 ,step: 2000 ,recorded: "2023/01/02" )
+# Record.create(user_id: 1 ,step: 3000 ,recorded: "2023/01/03" )
+# Record.create(user_id: 1 ,step: 4000 ,recorded: "2023/01/04" )
+# Record.create(user_id: 1 ,step: 5000 ,recorded: "2023/01/05" )
+# Record.create(user_id: 1 ,step: 4000 ,recorded: "2023/01/06" )
+# Record.create(user_id: 1 ,step: 5000 ,recorded: "2023/01/07" )
 
 # Level.create(id: 1 ,level_name: "初級" ,goal_step: 4000 ,level_stop: "なし")
 # Level.create(id: 2 ,level_name: "中級" ,goal_step: 6000 ,level_stop: "なし")
